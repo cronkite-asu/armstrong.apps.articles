0c4c2a2e54f8510bbf916b0b20cd275baa3fe258
micah <dataconcise@gmail.com>
2012-07-31 15:07:38 -0700

    add related content field 	modified:   armstrong/apps/articles/models.py

--- armstrong/apps/articles/models.py
+++ armstrong/apps/articles/models.py
@@ -1,12 +1,13 @@
 from armstrong.apps.content.models import Content
 from armstrong.core.arm_content.mixins.publication import PublishedManager
 from django.db import models
-
+from armstrong.apps.related_content.fields import RelatedObjectsField
 
 class Article(Content):
     body = models.TextField()
     home_summary = models.TextField(blank=True, null=True)
     home_title = models.CharField(max_length=100, blank=True, null=True)
+    related = RelatedObjectsField()
 
     objects = models.Manager()
     published = PublishedManager()
